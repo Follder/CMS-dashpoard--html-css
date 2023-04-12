f94a5c58278d86272a8968b01656fc66ad5779a2
DKoriahin <Dkoryahin@gmail.com>
2023-04-12 15:23:49 +0300

    adaptation project to all size screens

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    //burger menu
+    const burger = document.querySelector('.burger'),
+        menu = document.querySelector('.side-panel');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger_active');
+        menu.classList.toggle('side-panel_active');
+    });
 });
